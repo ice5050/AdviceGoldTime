1ea8db4507cf3677329e400eca033ab344c5241a
Ice5050 <ice50505050@hotmail.com>
2017-04-20 21:44:43 +0800

    Add showing result

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const argv = require('yargs').argv
 const R = require('ramda')
 
 const urls = argv._
+const scraper = scraperjs.StaticScraper.create
 const allP = proms => Promise.all(proms)
 const scrape = R.curry((fn, x) => x.scrape(fn))
 const then = R.curry((fn, x) => x.then(fn))
 const hasGoldTime = str => str.includes('gold-time-big.png')
 
-const scraper = scraperjs.StaticScraper.create
-
 R.compose(
+  then(R.tap(console.log)),
+  then(R.zip(urls)),
   then(R.map(imgs => imgs.some(hasGoldTime))),
   allP,
   R.map(scrape($ => $('#pri-pro img').map((_, img) => $(img).attr('src')).get())),
