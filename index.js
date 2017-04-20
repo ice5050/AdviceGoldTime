79440a234b3801faae7129d2ff02f1f0ff3601d1
Ice5050 <ice50505050@hotmail.com>
2017-04-20 22:12:10 +0800

    Format result in console

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const scraperjs = require('scraperjs')
 const argv = require('yargs').argv
 const R = require('ramda')
+const chalk = require('chalk')
 
 const urls = argv._
 const scraper = scraperjs.StaticScraper.create
@@ -8,9 +9,11 @@ const allP = proms => Promise.all(proms)
 const scrape = R.curry((fn, x) => x.scrape(fn))
 const then = R.curry((fn, x) => x.then(fn))
 const hasGoldTime = str => str.includes('gold-time-big.png')
+const toCheck = x => (x ? chalk.green('✔') : chalk.red('✘'))
+const toResult = res => `${toCheck(res[1])}\t${res[0]}`
 
 R.compose(
-  then(R.tap(console.log)),
+  then(R.tap(R.map(R.compose(console.log, toResult)))),
   then(R.zip(urls)),
   then(R.map(imgs => imgs.some(hasGoldTime))),
   allP,
